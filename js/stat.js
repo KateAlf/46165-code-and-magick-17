14d13a88b6260c4996f17627f77ae2bca6e85d48
KateAlf <katrin.alf@gmail.com>
2019-06-15 13:30:15 +0400

    Вносит правки после PR

--- js/stat.js
+++ js/stat.js
@@ -2,15 +2,19 @@
 
 var WIDTH_CLOUD = 420;
 var HEIGHT_CLOUD = 270;
-var WIDTH_BAR = 40;
-var HEIGHT_BAR = 150;
-var GAP_BAR = 50;
 var CLOUD_X = 100;
 var CLOUD_Y = 10;
-var GAP = 10;
-var COLOR_TEXT = '#000000';
+
+var WIDTH_BAR = 40;
+var HEIGHT_BAR = 150;
 var BAR_Y = 100;
+
+var GAP_BAR = 50;
+var GAP = 10;
+
+var COLOR_TEXT = '#000000';
 var NAME_Y = 270;
+var USER_NAME = 'Вы';
 
 var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
@@ -29,6 +33,13 @@ var getMaxElement = function (arr) {
   return maxElement;
 };
 
+// Функция генерации случайного числа
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+
 window.renderStatistics = function (ctx, names, times) {
   renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, '#ffffff');
@@ -44,15 +55,19 @@ window.renderStatistics = function (ctx, names, times) {
 
   for (var i = 0; i < names.length; i++) {
     ctx.fillStyle = COLOR_TEXT;
-    ctx.fillText(names[i], CLOUD_X + WIDTH_BAR + (WIDTH_BAR + GAP_BAR) * i, NAME_Y);
+    var nameX = CLOUD_X + WIDTH_BAR + (WIDTH_BAR + GAP_BAR) * i;
+    ctx.fillText(names[i], nameX, NAME_Y);
     
-    if (names[i] === 'Вы') {
-    ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+    if (names[i] === USER_NAME) {
+      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
     } else {
-    ctx.fillStyle = colorsBar[i];
-  };
+      ctx.fillStyle = colorsBar[i];
+    }
     
     var heightDynamic = HEIGHT_BAR * times[i] / maxTime;
-    ctx.fillRect(CLOUD_X + WIDTH_BAR + (WIDTH_BAR + GAP_BAR) * i, BAR_Y + (HEIGHT_BAR - heightDynamic), WIDTH_BAR, heightDynamic);
+    var barX = CLOUD_X + WIDTH_BAR + (WIDTH_BAR + GAP_BAR) * i;
+    var barY = BAR_Y + (HEIGHT_BAR - heightDynamic);
+    
+    ctx.fillRect(barX, barY, WIDTH_BAR, heightDynamic);
   }
 };
